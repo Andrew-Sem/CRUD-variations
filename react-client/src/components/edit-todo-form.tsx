a390465ab6781e64aa504e2ee00eaa4a118c2560
Andrew Semyonov <101043571+Andrew-Sem@users.noreply.github.com>
2023-12-11 12:26:28 +0300

    todo ui

---
+++ react-client/src/components/edit-todo-form.tsx
@@ -0,0 +1,76 @@
+import { todoFormSchema } from '@/lib/validations/todo';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { Button } from '@/components/ui/button';
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Textarea } from './ui/textarea';
+
+export const EditTodoForm = ({
+	closeDialog,
+	defaultTitle,
+	defaultDescription,
+}: {
+	closeDialog: () => void;
+	defaultTitle: string;
+	defaultDescription: string;
+}) => {
+	const form = useForm<z.infer<typeof todoFormSchema>>({
+		resolver: zodResolver(todoFormSchema),
+		defaultValues: {
+			title: defaultTitle,
+			description: defaultDescription,
+		},
+	});
+	const onSubmit = (values: z.infer<typeof todoFormSchema>) => {
+		console.log(values);
+		closeDialog();
+	};
+	return (
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+				<FormField
+					control={form.control}
+					name='title'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Title</FormLabel>
+							<FormControl>
+								<Input placeholder='I want to do...' {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name='description'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Description</FormLabel>
+							<FormControl>
+								<Textarea placeholder='Type description here' {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<div className='flex space-x-4 justify-end'>
+					<Button type='reset' variant={'secondary'} onClick={closeDialog}>
+						Cancel
+					</Button>
+					<Button type='submit'>Save changes</Button>
+				</div>
+			</form>
+		</Form>
+	);
+};
--- react-client/src/components/todo.tsx
+++ react-client/src/components/todo.tsx
@@ -1,11 +1,74 @@
 import { TodoType } from '@/types/Todo';
 import { TableCell, TableRow } from './ui/table';
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from '@/components/ui/dialog';
+import { PenBoxIcon, XIcon } from 'lucide-react';
+import { Button } from './ui/button';
+import { EditTodoForm } from './edit-todo-form';
+import { useState } from 'react';
+import { DialogClose } from '@radix-ui/react-dialog';
 
 export const Todo = ({ todo }: { todo: TodoType }) => {
+	const [openEditDialog, setOpenEditDialog] = useState(false);
 	return (
-		<TableRow className=''>
+		<TableRow className=' '>
 			<TableCell>{todo.title}</TableCell>
 			<TableCell>{todo.description}</TableCell>
+			<TableCell className='w-4'>
+				<Dialog open={openEditDialog} onOpenChange={setOpenEditDialog}>
+					<DialogTrigger asChild>
+						<Button variant={'ghost'} size={'sm'}>
+							<PenBoxIcon className='w-4 h-4' />
+						</Button>
+					</DialogTrigger>
+					<DialogContent>
+						<DialogHeader>
+							<DialogTitle>Edit todo</DialogTitle>
+							<DialogDescription>
+								Make changes to your todo here. Click save when you're done.
+							</DialogDescription>
+						</DialogHeader>
+						<EditTodoForm
+							closeDialog={() => setOpenEditDialog(false)}
+							defaultTitle={todo.title}
+							defaultDescription={todo.description}
+						/>
+					</DialogContent>
+				</Dialog>
+			</TableCell>
+			<TableCell className='w-4'>
+				<Dialog>
+					<DialogTrigger asChild>
+						<Button variant={'ghost'} size={'sm'} className='hover:bg-destructive'>
+							<XIcon className='w-4 h-4' />
+						</Button>
+					</DialogTrigger>
+					<DialogContent>
+						<DialogHeader>
+							<DialogTitle>Are you sure absolutely sure?</DialogTitle>
+							<DialogDescription>
+								This action cannot be undone. This will permanently remove this todo
+								from our servers.
+							</DialogDescription>
+						</DialogHeader>
+						<DialogFooter className='flex space-x-3 justify-end'>
+							<DialogClose>
+								<Button variant={'secondary'}>Cancel</Button>
+							</DialogClose>
+							<DialogClose>
+								<Button>Delete todo</Button>
+							</DialogClose>
+						</DialogFooter>
+					</DialogContent>
+				</Dialog>
+			</TableCell>
 		</TableRow>
 	);
 };
